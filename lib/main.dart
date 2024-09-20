5b6243ef568d145c323bfc0457dcc4780105d2e0
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-09-20 14:42:58 +0300

    add localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:halal/halal_app.dart';
 
-void main() {
-  runApp(const HalalApp());
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await EasyLocalization.ensureInitialized();
+
+  runApp(
+     EasyLocalization(
+        supportedLocales: [const Locale('en', 'US'), const Locale('ar', 'EG')],
+        path: 'assets/translations', // <-- change the path of the translation files
+       saveLocale: true,
+       startLocale: const Locale('ar', 'EG'),
+     child:   const HalalApp()),
+  );
 }
