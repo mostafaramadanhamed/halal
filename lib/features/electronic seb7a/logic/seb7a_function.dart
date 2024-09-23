c875fb3dc25a5aede2931924363096c3a7638724
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-09-23 12:15:35 +0300

    add seb7a functions controller

---
+++ lib/features/electronic seb7a/logic/seb7a_function.dart
@@ -0,0 +1,14 @@
+class Seb7aFunctionController {
+  static void increamentCounter(int counter) {
+    counter++;
+  }
+
+  static void decreamentCounter(int counter) {
+    counter--;  
+  }
+
+  static void resetCounter(int counter) {
+    counter = 0;
+  }
+
+}
