958a63f9df4ac5b204624ae4dca830fbd44a9496
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-09-24 12:50:55 +0300

    add azkar data filtter

---
+++ lib/features/azkar/data/azkar_categories.dart
@@ -0,0 +1,24 @@
+import 'package:halal/features/azkar/data/azkar_data.dart';
+
+List<Map<String, dynamic>> azkarCategories = [
+  {
+    "title":"أذكار الصباح والمساء",
+    "total":1,
+    "data":azkarAlsaba7,
+  },
+  {
+    "title":"أذكار النوم",
+    "total":2,
+    "data":azkarSleeping,
+  },
+  {
+    "title":"أذكار الوضوء",
+    "total":2,
+    "data":azkarAblution,
+  },
+  {
+    "title":"أذكار الصلاة",
+    "total":1,
+    "data":azkarPrayer,
+  },
+];
